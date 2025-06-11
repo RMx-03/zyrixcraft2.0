896081599a1d17164db62c511dd34d8e05b42875
RMx-03 <140522220+RMx-03@users.noreply.github.com>
2025-06-11 12:10:48 +0530

    Portfolio link update

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -16,7 +16,7 @@ const serviceData = [
     title: "Web Projects",
     description: "We craft responsive, high-performance websites that align with your brand and deliver seamless user experiences across all devices. Our development approach ensures optimal performance and security.",
     icon: <FaCode />,
-    link: "https://show-case-web-site-awt4.vercel.app/",
+    link: "https://zyrixcraft-project-showcase.vercel.app/",
     imageUrl: "https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1160&q=80",
     imageAlt: "Web Development",
   },
